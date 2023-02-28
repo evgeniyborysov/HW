b2d20cd8e1cac22743cd6073a036a06146108d42
evgeniy borysov <evgeniy.borysov@gmail.com>
2023-02-28 16:07:13 +0200

    hm8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,52 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+	| { type: "sort"; payload: "up" | "down" }
+	| { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (
+	state: UserType[],
+	action: ActionType
+): UserType[] => {
+	// need to fix any
+	switch (action.type) {
+		case "sort": {
+			let copyState = [...state];
 
-            return state // need to fix
-        }
-        case 'check': {
+			// by name
+			if (action.payload === "up") {
+				copyState = copyState.sort(function (a, b) {
+					if (a.name > b.name) {
+						return 1;
+					}
+					if (a.name < b.name) {
+						return -1;
+					}
+					return 0;
+				});
+			}
+			if (action.payload === "down") {
+				copyState = copyState.sort(function (a, b) {
+					if (a.name < b.name) {
+						return 1;
+					}
+					if (a.name > b.name) {
+						return -1;
+					}
+					return 0;
+				});
+			}
 
-            return state // need to fix
-        }
-        default:
-            return state
-    }
-}
+			return copyState; // need to fix
+		}
+		case "check": {
+			let copyState = [...state];
+			copyState = copyState.filter(
+				(people) => people.age >= action.payload
+			);
+			return copyState; // need to fix
+		}
+		default:
+			return state;
+	}
+};
